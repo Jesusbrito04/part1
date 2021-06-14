7da3b9fc6a69d04382d18772bc23aa31c15b61dd
Diego Brito <britodiego.tech@gmail.com>
2021-06-14 12:04:50 -0400

    Courseinfo Finish

--- courseinfo/src/index.js
+++ courseinfo/src/index.js
@@ -1,17 +1,58 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const Header = ({ namee }) => <div>Courses: {namee}</div>;
+
+const Content = ({ parts, exercises }) => (
+  <div>
+    <p>
+      {parts} {exercises}
+    </p>
+  </div>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Total = ({ sum }) => <div>Total: {sum}</div>;
+
+const App = () => {
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
+
+  const namee = course.name;
+
+  const part = course.parts.map((agar) => (
+    <div>
+      <p>
+        {agar.name} {agar.exercises}
+      </p>
+    </div>
+  ));
+
+  const sum = course.parts
+    .map((task) => task.exercises)
+    .reduce((a, b) => a + b);
+
+  return (
+    <div>
+      <Header namee={namee} />
+      <Content parts={part} />
+      <Total sum={sum} />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
